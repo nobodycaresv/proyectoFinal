6507b721e1f55383f8cc0c8e794064f0c4b29256
FranT-1“ <franciscotamer09@gmail.com>
2026-08-30 20:19:57 -0300

    Agrego componente Main con estilos

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import estilos from './Main.module.css';
 
-export default function Main() {
+export default function Presentacion() {
+  const handleServicios = () => {
+    alert('Botón 1 presionado');
+  };
 
+  const handleContactanos = () => {
+    alert('Botón 2 presionado');
+  };
 
-    return (
-        <div>
-            <div class="card">
-                <div class="card-body">
-                    This is some text within a card body.
-                </div>
-            </div>
+  return (
+    <div className={estilos.contenedor}>
+      <div className={estilos.card}>
+        <h1 className={estilos.titulo}>ROSARIO ESTÉTICA VEHICULAR</h1>
+        <p className={estilos.descripcion}>
+        Somos una empresa que se dedica exclusivamente al detallado y estética automotriz.
+        Solo tienes que traer tu vehiculo y nosotros nos encargamos del resto.
+        </p>
+        <div className={estilos.botones}> 
+          <button className={`${estilos.btn} ${estilos.btnPrimario}`} onClick={handleServicios}>Nuestros Servicios</button>
+          <button className={`${estilos.btn} ${estilos.btnSecundario}`} onClick={handleContactanos}>Contactanos</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
